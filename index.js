2d7f0295d2b672792beca5586579c3661e3624f2
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2023-01-10 16:43:52 +0700

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-  	const express = require('express'),
-  	     http = require('http');
+  	// const express = require('express'),
+  	//      http = require('http');
   	
-  	const hostname = 'localhost';
-  	const port = 5000;
+  	// const hostname = 'localhost';
+  	// const port = 5000;
   	
-  	const app = express();
+  	// const app = express();
   	
-  	app.use((req, res, next) => {
-  	  console.log(req.headers);
-  	  res.statusCode = 200;
-  	  res.setHeader('Content-Type', 'text/html');
-  	  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  	// app.use((req, res, next) => {
+  	//   console.log(req.headers);
+  	//   res.statusCode = 200;
+  	//   res.setHeader('Content-Type', 'text/html');
+  	//   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
   	
-  	});
+  	// });
   	
-  	const server = http.createServer(app);
+  	// const server = http.createServer(app);
   	
-  	server.listen(port, hostname, () => {
-  	  console.log(`Server running at http://${hostname}:${port}/`);
-  	});
+  	// server.listen(port, hostname, () => {
+  	//   console.log(`Server running at http://${hostname}:${port}/`);
+  	// });
+
+
+      const express = require("express");
+      const morgan = require("morgan");
+      const http = require("http");
+      const hostname = "localhost";
+      const port = 5000;
+      
+      const app = express();
+      app.use(morgan("dev"));
+      app.use(morgan("dev"));
+      app.use((req, res, next) => {
+        console.log(req.headers);
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+      });
+      
+      const server = http.createServer(app);
+      
+      server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+      });
